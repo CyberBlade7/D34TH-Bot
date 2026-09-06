32215854dfcbe40317c0d1e506d5a3e2d7d45da4
CyberBlade7 <154821695+CyberBlade7@users.noreply.github.com>
2026-09-06 07:53:01 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        state: "EDITING",     // this is what people actually see
+        type: 0,               // Custom
       },
     ],
   },
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#762030",
+      secondary: "#b8324d",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
